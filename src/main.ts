9e281b07f3b52a9f0b08d327fe14273acc19dc7a
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-06-19 18:58:44 +0800

    Use arrow function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async bootstrap() = async => {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
