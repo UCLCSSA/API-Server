383667d25c60b6b4f8d3808db0934adc5b6fa607
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-06-19 21:26:36 +0800

    Add some security middleware and rate limiting settings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
 import { NestFactory } from '@nestjs/core';
+
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
+
 import { AppModule } from './app.module';
 
-async bootstrap() = async => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+// Rate limit parameters *would* ideally be loaded via the `config` module,
+// but that module is loaded AFTER the rate limiting middleware is loaded.
+// Hence, we must manually obtain the two config parameters from `process.env`.
+const RATE_LIMIT_WINDOW_MS: number =
+    parseInt(process.env.RATE_LIMIT_WINDOW_MS, 10);
+
+const RATE_LIMIT_MAX: number =
+    parseInt(process.env.RATE_LIMIT_WINDOW_MS, 10);
+
+if (isNaN(RATE_LIMIT_WINDOW_MS) || isNaN(RATE_LIMIT_MAX)) {
+    throw new Error('Invalid rate limit settings!');
 }
 
+const bootstrap = async () => {
+    const app = await NestFactory.create(AppModule);
+
+    // CORS, CSRF policies
+    app.enableCors();
+    app.use(helmet());
+    app.use(csurf());
+
+    // Rate limiting
+    app.use(rateLimit({
+        windowMs: RATE_LIMIT_WINDOW_MS,
+        max: RATE_LIMIT_MAX,
+    }));
+
+    await app.listen(3000);
+};
+
+// noinspection JSIgnoredPromiseFromCall
 bootstrap();
