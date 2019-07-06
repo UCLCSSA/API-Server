6b6c09e159d7a6e93c4ea046c0735df5b86ff241
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-07-06 16:21:30 +0800

    Clarify with comments

--- src/auth/registration/helpers/uclcssa-session-key-generator.ts
+++ src/auth/registration/helpers/uclcssa-session-key-generator.ts
@@ -2,22 +2,32 @@ import { Injectable } from '@nestjs/common';
 
 import * as crypto from 'crypto';
 
-/*
- * UCLCSSA Session keys are intended to be 128-bits.
- * Each key is generated from the information:
- *  - WeChat openId
- *  - WeChat sessionKey
- *  - A 32-byte secure random number as secret key
- */
-
 @Injectable()
 export class UclcssaSessionKeyGenerator {
     generateUclcssaSessionKey(
         openId: string, sessionKey: string,
     ): Promise<string> {
         return new Promise((resolve, reject) => {
+            // The core data is composed of openId concatenated with sessionKey.
+            // These data are obtained from WeChat API. We cannot return
+            // WeChat's sessionKey directly due to client-side security issues.
+            // Instead, we need to generate a new uclcssaSessionKey to act as a
+            // proxy - there is a one-to-one correspondence between
+            // uclcssaSessionKey <-> sessionKey. Requests made to WeChat APIs
+            // require the client to return uclcssaSessionKey, then the backend
+            // service will query records, and if a matching record is found,
+            // the backend service will use the corresponding WeChat sessionKey
+            // to retrieve WeChat API information on behalf of the user.
             const payload = openId + sessionKey;
 
+            // A 256-bit cryptographically-secure random number is generated
+            // and is used as the SHA-256 key which is used to hash the payload
+            // in order to generate a uclcssaSessionKey. This random number is
+            // necessary to guarantee uniqueness of uclcssaSessionKey with
+            // respect to time. This means that a uclcssaSessionKey may be
+            // assigned a validity period and that when the client registers
+            // again, a different uclcssaSessionKey is generated as a
+            // result, invalidating the old uclcssaSessionKey.
             crypto.randomBytes(32, (err, buffer) => {
                 if (err) {
                     reject(err);
@@ -25,6 +35,9 @@ export class UclcssaSessionKeyGenerator {
                     const random = buffer.toString('hex');
                     const hmac = crypto.createHmac('sha256', random);
                     hmac.update(payload);
+                    // Hex (base 16) encoding is used to encode the generated
+                    // 256-bit uclcssaSessionKey, meaning that the
+                    // uclcssaSessionKey is 16 hex digits long.
                     const uclcssaSessionKey = hmac.digest('hex');
                     resolve(uclcssaSessionKey);
                 }
