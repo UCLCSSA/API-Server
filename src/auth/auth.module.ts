2b4fa692a6462f4f91097f5c810d318f0a09c8c2
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-07-13 21:15:38 +0800

    Register RandomSha256Generator as a provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '../config/config.module';
 
 import { UserSession } from './registration/entities/user-session.entity';
+import { RandomSha256Generator } from './registration/helpers/random-sha256-generator';
 import { UclcssaSessionKeyGenerator } from './registration/helpers/uclcssa-session-key-generator';
 import { RegistrationController } from './registration/registration.controller';
 import { RegistrationService } from './registration/registration.service';
@@ -17,6 +18,7 @@ import { WeChatAuthService } from './registration/helpers/wechat-auth.service';
         WeChatAuthService,
         UserSessionService,
         UclcssaSessionKeyGenerator,
+        RandomSha256Generator,
     ],
 })
 export class AuthenticationModule {}
