a83a91da7a09e3db3ac0937d8d839510879778fb
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-07-18 16:47:00 +0800

    Use logging middleware

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 
-import config from '../config/config';
+import config from './config/config';
+
+import createLogger from './log/logger';
 
 const app = express();
 
@@ -24,6 +26,17 @@ const rateLimiter = rateLimit({
 });
 
 app.use(rateLimiter);
+
+// Request logging
+const logger = createLogger({
+    fileName: config.get('logging.fileName'),
+    basePath: config.get('logging.basePath'),
+    interval: config.get('logging.interval'),
+    fileSize: config.get('logging.fileSize'),
+    compressionMethod: config.get('logging.compressionMethod'),
+});
+
+app.use(logger);
 
 // The port the server is to listen on. Defaults to 3000.
 const port = config.get('port');
--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-import config from 'config';
-import { expect } from 'chai';
-import server from '../src/index';
-
-describe('Server', () => {
-    it('runs on desired port', () => {
-        expect(server.port).to.equal(config.get('port'));
-    });
-});
