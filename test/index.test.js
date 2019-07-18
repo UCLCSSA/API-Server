5384bddfb60f991ca46a32f98df937af67220b17
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-07-18 14:57:42 +0800

    Format files

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,9 @@
-import {expect} from 'chai';
-
 import config from 'config';
-
+import { expect } from 'chai';
 import server from '../src/index';
 
 describe('Server', () => {
-    it('runs on desired port', async () => {
+    it('runs on desired port', () => {
         expect(server.port).to.equal(config.get('port'));
     });
 });
