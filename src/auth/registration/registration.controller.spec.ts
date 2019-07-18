68e80e32ccbbf955d277f2fe60447b6aefca942f
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-07-18 12:34:01 +0800

    Remove file

--- src/auth/registration/registration.controller.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RegistrationController } from '../../../src/auth/registration/registration.controller';
-import { RegistrationService } from '../../../src/auth/registration/registration.service';
-
-describe('RegistrationController', () => {
-    let registrationController: RegistrationController;
-    let registrationService: RegistrationService;
-});
