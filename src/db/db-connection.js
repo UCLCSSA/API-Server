e21b9276abe64f9c77e8e22bb5365301b18e8db6
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-07-21 16:54:31 +0800

    Rename

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -7,8 +7,10 @@ const dbOptionsAreValid = ({ host, userName, userPassword, databaseName }) =>
     && isNonEmptyString(databaseName);
 
 const createDbConnection = dbFactory => dbOptions => {
-    if (!dbFactory || !dbFactory.createConnection) throw Error('Invalid connection factory.');
-    if (!dbOptionsAreValid(dbOptions)) throw new Error('Invalid database options.');
+    if (!dbFactory || !dbFactory.createConnection)
+        throw Error('Invalid connection factory.');
+    if (!dbOptionsAreValid(dbOptions))
+        throw new Error('Invalid database options.');
 
     const { host, userName, userPassword, databaseName } = dbOptions;
     return dbFactory.createConnection({
--- src/db/db-connection.spec.js
+++ src/db/db-connection.spec.js
@@ -7,9 +7,9 @@ describe('createDbConnection', () => {
     it('should create database connection with user supplied options', () => {
         const dbOptions = {
             host: 'host',
-            user: 'user',
-            password: 'secret',
-            database: 'hello_world_db',
+            userName: 'user',
+            userPassword: 'secret',
+            databaseName: 'hello_world_db',
         };
 
         const fakeDbFactory = { createConnection: sinon.fake() };
@@ -17,16 +17,21 @@ describe('createDbConnection', () => {
         createDbConnection(fakeDbFactory)(dbOptions);
 
         expect(fakeDbFactory.createConnection.calledOnce).to.equal(true);
-        expect(fakeDbFactory.createConnection.args[0][0]).to.deep.equal(dbOptions);
+
+        const result = fakeDbFactory.createConnection.args[0][0];
+        expect(result.host).to.equal(dbOptions.host);
+        expect(result.user).to.equal(dbOptions.userName);
+        expect(result.password).to.equal(dbOptions.userPassword);
+        expect(result.database).to.equal(dbOptions.databaseName);
     });
 
     it('should throw error if invalid database options are given', () => {
         const invalidDbOptions = {
             // empty options are not allowed.
             host: '',
-            user: null,
-            password: 'secret',
-            database: 'hello_world_db',
+            userName: null,
+            userPassword: 'secret',
+            databaseName: 'hello_world_db',
         };
 
         const fakeDbFactory = { createConnection: sinon.fake() };
