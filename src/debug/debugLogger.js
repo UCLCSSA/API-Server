659cd88d4e12ab4d9f9234993bc2b68aa5d31aa1
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-08-02 21:47:03 +0800

    Remove attempt to log response

--- src/debug/debugLogger.js
+++ src/debug/debugLogger.js
@@ -1,16 +1,11 @@
 import debug from './debug';
 
 const debugLogger = (req, res, next) => {
-  debug('========= <REQ/RES> =========');
+  debug('=============================');
   debug('[REQUEST headers]');
   debug(req.headers);
   debug('[REQUEST body]');
   debug(req.body);
-  debug('---------');
-  debug('[RESPONSE headers]');
-  debug(res.headers);
-  debug('[RESPONSE body]');
-  debug(res.body);
   debug('=============================');
   next();
 };
