a802699870b84864e64bdb7d820ac81c5cebc617
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-08-02 15:29:47 +0800

    Add auth via wechat dependency for handler

--- src/auth/registration/registration.router.js
+++ src/auth/registration/registration.router.js
@@ -2,9 +2,13 @@ import express from 'express';
 
 import createWechatRegistrationHandler from './wechat-registration.handler';
 
+import authenticateViaWechat
+  from 'src/auth/registration/helpers/authenticate-via-wechat';
+
 const registrationRouter = express.Router();
 
-const wechatRegistrationHandler = createWechatRegistrationHandler()();
+const wechatRegistrationHandler =
+  createWechatRegistrationHandler(authenticateViaWechat)(null);
 
 registrationRouter.post('/register/wechat', wechatRegistrationHandler);
 
