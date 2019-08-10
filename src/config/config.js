057e43b764345777defd5e0d900fdc46620f8326
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-08-10 18:04:07 +0800

    Remove express rate limiting
    Rate limiting and throttling is better handled by a reverse proxy such
    as nginx. Express should not be the server responsible for handling
    rate limiting and throttling.

--- src/config/config.js
+++ src/config/config.js
@@ -22,21 +22,6 @@ const config = convict({
     default: 3000,
     env: 'PORT'
   },
-  rateLimit: {
-    doc: 'Rate limiting settings.',
-    windowMs: {
-      doc: 'Time until request count is reset (milliseconds).',
-      format: 'int',
-      default: 5 * 60 * 1000,
-      env: 'RATE_LIMIT_WINDOW_MS'
-    },
-    windowMax: {
-      doc: 'Max requests per window period.',
-      format: 'int',
-      default: 100,
-      env: 'RATE_LIMIT_WINDOW_MAX'
-    }
-  },
   logging: {
     doc: 'Log file settings.',
     fileName: {
